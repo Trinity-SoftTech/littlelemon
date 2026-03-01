67a77f908082e025d230c3d1520a978495f76ccb
Amakundi Samuel Bako <amakondisamuel@gmail.com>
2026-03-02 00:24:03 +0100

    Last Commit

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -28,8 +28,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/', include(router.urls)),
-    path('restaurant/', include('restaurant.urls')),
-    path('restaurant/booking/', include(router.urls)),
+    path('api/restaurant/', include('restaurant.urls')),
+    path('api/restaurant/booking/', include(router.urls)),
     
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken'))
